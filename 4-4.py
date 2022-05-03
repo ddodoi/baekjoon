bed1ff82661294468a5dd9aa8217863541c177eb
ddodoi <99958318+ddodoi@users.noreply.github.com>
2022-05-04 07:53:04 +0900

    Add files via upload
    백준 1차원 배열
    4-4 
    문제풀이중

---
+++ 4-4.py
@@ -0,0 +1,24 @@
+arr=[]
+
+for i in range(10):
+    arr.append(int(input()))
+
+num=[]
+for i in range(10):
+    num.append(arr[i]/42)
+
+k=0
+
+for i in range(10):
+    if (num[0]!=num[i+1]):
+        k+=1
+
+print(k)
+-------------------------------------------------------
+arr=[]
+Arr=[]
+for i in range(10):
+    arr.append(int(input()))
+    Arr[i]=(arr[i]/42)
+
+print(Arr)
