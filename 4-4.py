59b27d3672209623fd604ce76bb3cf9c2e6dfc32
ddodoi <99958318+ddodoi@users.noreply.github.com>
2022-05-09 13:54:09 +0900

    Update 4-4.py

--- 4-4.py
+++ 4-4.py
@@ -22,3 +22,20 @@ for i in range(10):
     Arr[i]=(arr[i]/42)
 
 print(Arr)'''
+
+'''arr=[]
+
+for i in range(10):
+    arr.append(int(input()))
+
+num=[]
+for i in range(10):
+    num.append(arr[i]/42)
+
+k=0
+
+for i in range(10):
+    if (num[0]!=num[i+1]):
+        k+=1
+
+print(k)'''
